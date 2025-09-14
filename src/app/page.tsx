dc2ebadde047357677254941364daa821e92c72e
Lameck WH Mbewe <68487945+Lameckwh@users.noreply.github.com>
2025-09-14 10:50:17 +0200

    Bump chart version to 0.1.1 and add imagePullSecrets; create values-dev.yaml for development environment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-background text-foreground p-8">
       <h1 className="text-4xl font-bold mb-4">Welcome to My Next.js App!</h1>
       <p className="text-lg">This is a simple app deployed on Github via a CI/CD pipeline.</p>
-      <p className="text-lg">Completed the tekton triggers setup</p>
+      <p className="text-lg">Deployed on development environment</p>
     </main>
   );
 }
